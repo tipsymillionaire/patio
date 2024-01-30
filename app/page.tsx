992de5fa5ba3f18bda9a9e65be698d7fc030e39d
tipsymillionaire <74567870+tipsymillionaire@users.noreply.github.com>
2024-01-30 03:58:57 +0200

    Added Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/home/hero";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello world!</h1>
+    <main className="">
+      <Hero />
     </main>
   );
 }
